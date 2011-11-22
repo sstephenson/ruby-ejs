21cb52e1ac29e1405b0e231fc9a1f0291d5ebb14
Alexis Bernard <alexis@official.fm>
2011-11-22 10:40:30 +0100

    Fix tests (Chab's private tester).

--- test/test_ejs.rb
+++ test/test_ejs.rb
@@ -5,12 +5,14 @@ FUNCTION_PATTERN = /^function\s*\(.*?\)\s*\{(.*?)\}$/
 
 BRACE_SYNTAX = {
   :evaluation_pattern    => /\{\{([\s\S]+?)\}\}/,
-  :interpolation_pattern => /\{\{=([\s\S]+?)\}\}/
+  :interpolation_pattern => /\{\{:([\s\S]+?)\}\}/,
+  :interpolation_safe_pattern => /\{\{=([\s\S]+?)\}\}/
 }
 
 QUESTION_MARK_SYNTAX = {
   :evaluation_pattern    => /<\?([\s\S]+?)\?>/,
-  :interpolation_pattern => /<\?=([\s\S]+?)\?>/
+  :interpolation_pattern => /<\?:([\s\S]+?)\?>/,
+  :interpolation_safe_pattern => /<\?=([\s\S]+?)\?>/
 }
 
 module TestHelper
@@ -29,6 +31,14 @@ class EJSCompilationTest < Test::Unit::TestCase
   end
 
   test "compile with custom syntax" do
+    standard_result = EJS.compile("Hello <%: name %>")
+    braced_result   = EJS.compile("Hello {{: name }}", BRACE_SYNTAX)
+
+    assert_match FUNCTION_PATTERN, braced_result
+    assert_equal standard_result, braced_result
+  end
+
+  test "compile safe tags with custom syntax " do
     standard_result = EJS.compile("Hello <%= name %>")
     braced_result   = EJS.compile("Hello {{= name }}", BRACE_SYNTAX)
 
@@ -41,14 +51,17 @@ class EJSCustomPatternTest < Test::Unit::TestCase
   extend TestHelper
 
   def setup
+    @original_safe_interpolation_pattern = EJS.interpolation_safe_pattern
     @original_evaluation_pattern = EJS.evaluation_pattern
     @original_interpolation_pattern = EJS.interpolation_pattern
     EJS.evaluation_pattern = BRACE_SYNTAX[:evaluation_pattern]
     EJS.interpolation_pattern = BRACE_SYNTAX[:interpolation_pattern]
+    EJS.interpolation_safe_pattern = BRACE_SYNTAX[:interpolation_safe_pattern]
   end
 
   def teardown
     EJS.interpolation_pattern = @original_interpolation_pattern
+    EJS.interpolation_safe_pattern = @original_safe_interpolation_pattern
     EJS.evaluation_pattern = @original_evaluation_pattern
   end
 
